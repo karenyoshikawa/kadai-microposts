ef60f92fbf7a82a6328495fc8120e7ded3f578cd
karen.yoshikawa <karen.yoshikawa@rakuten.com>
2018-06-19 06:29:36 +0000

    follow, unfollow

--- .c9/metadata/environment/healthylife/app/Http/Controllers/Controller.php
+++
@@ -1,1 +0,0 @@
-{"filter":false,"title":"Controller.php","tooltip":"/healthylife/app/Http/Controllers/Controller.php","ace":{},"undoManager":{"mark":-1,"position":-1,"stack":[]},"timestamp":1514998335000,"hash":"080a728cc40011ab4c4a68d88e221ed8a577d562"}
--- jikken.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $fruits = ['リンゴ', 'バナナ', 'オレンジ', 'ぶどう', '桃'];
-    $count = count($fruits);         // assign the number of items in the array to $count
-    for ($i = 0; $i < $count; $i++) {
-        print mt_rand(1,10) . '回目: ' . $fruits[$i] . PHP_EOL;
-    }
-?>
